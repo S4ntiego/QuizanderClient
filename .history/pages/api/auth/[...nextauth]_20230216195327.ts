4dfceb1326f97ea580b900055ebc278e3dfa5846
S4ntiego <adamksiazek123@gmail.com>
2023-02-16 22:21:31 +0100

    first commit

---
+++ .history/pages/api/auth/[...nextauth]_20230216195327.ts
@@ -0,0 +1,21 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/lib/prisma";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          scope: "openid https://www.googleapis.com/auth/gmail.send",
+        },
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
